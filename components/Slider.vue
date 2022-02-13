75011336ded52853463aac23c32e77a3881f8eef
maipalash <maipalash35@gmail.com>
2022-02-13 17:51:39 +0600

    initial commit

---
+++ components/Slider.vue
@@ -0,0 +1,197 @@
+<template>
+    <section class="intro-section">
+        <div class="swiper-container swiper-theme nav-inner pg-inner swiper-nav-lg animation-slider pg-xxl-hide nav-xxl-show nav-hide">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide banner banner-fixed intro-slide intro-slide1" :style="{ backgroundImage: 'url(' + require('@/assets/images/demos/demo1/sliders/slide-1.jpg') + ')' }">
+                    <div class="container">
+                        <figure class="slide-image skrollable slide-animate">
+                            <img src="~/assets/images/demos/demo1/sliders/shoes.png" alt="Banner"
+                                data-bottom-top="transform: translateY(10vh);"
+                                data-top-bottom="transform: translateY(-10vh);" width="474" height="397">
+                        </figure>
+                        <div class="banner-content y-50 text-right">
+                            <h5 class="banner-subtitle font-weight-normal text-default ls-50 lh-1 mb-2 slide-animate"
+                                data-animation-options="{
+                            'name': 'fadeInRightShorter',
+                            'duration': '1s',
+                            'delay': '.2s'
+                        }">
+                                Custom <span class="p-relative d-inline-block">Men’s</span>
+                            </h5>
+                            <h3 class="banner-title font-weight-bolder ls-25 lh-1 slide-animate"
+                                data-animation-options="{
+                            'name': 'fadeInRightShorter',
+                            'duration': '1s',
+                            'delay': '.4s'
+                        }">
+                                RUNNING SHOES
+                            </h3>
+                            <p class="font-weight-normal text-default slide-animate" data-animation-options="{
+                            'name': 'fadeInRightShorter',
+                            'duration': '1s',
+                            'delay': '.6s'
+                        }">
+                                Sale up to <span class="font-weight-bolder text-secondary">30% OFF</span>
+                            </p>
+
+                            <a href="shop-list.html"
+                                class="btn btn-dark btn-outline btn-rounded btn-icon-right slide-animate"
+                                data-animation-options="{
+                            'name': 'fadeInRightShorter',
+                            'duration': '1s',
+                            'delay': '.8s'
+                        }">SHOP NOW<i class="w-icon-long-arrow-right"></i></a>
+
+                        </div>
+                        <!-- End of .banner-content -->
+                    </div>
+                    <!-- End of .container -->
+                </div>
+                <!-- End of .intro-slide1 -->
+
+                <div class="swiper-slide banner banner-fixed intro-slide intro-slide2" :style="{ backgroundImage: 'url(' + require('@/assets/images/demos/demo1/sliders/slide-2.jpg') + ')' }">
+                    <div class="container">
+                        <figure class="slide-image skrollable slide-animate" data-animation-options="{
+                            'name': 'fadeInUpShorter',
+                            'duration': '1s'
+                        }">
+                            <img src="~/assets/images/demos/demo1/sliders/men.png" alt="Banner"
+                                data-bottom-top="transform: translateX(10vh);"
+                                data-top-bottom="transform: translateX(-10vh);" width="480" height="633">
+                        </figure>
+                        <div class="banner-content d-inline-block y-50">
+                            <h5 class="banner-subtitle font-weight-normal text-default ls-50 slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInUpShorter',
+                                'duration': '1s',
+                                'delay': '.2s'
+                            }">
+                                Mountain-<span class="text-secondary">Climbing</span>
+                            </h5>
+                            <h3 class="banner-title font-weight-bolder text-dark mb-0 ls-25 slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInUpShorter',
+                                'duration': '1s',
+                                'delay': '.4s'
+                            }">
+                                Hot & Packback
+                            </h3>
+                            <p class="font-weight-normal text-default slide-animate" data-animation-options="{
+                                'name': 'fadeInUpShorter',
+                                'duration': '1s',
+                                'delay': '.8s'
+                            }">
+                                Only until the end of this week.
+                            </p>
+                            <a href="shop-banner-sidebar.html"
+                                class="btn btn-dark btn-outline btn-rounded btn-icon-right slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInUpShorter',
+                                'duration': '1s',
+                                'delay': '1s'
+                            }">
+                                SHOP NOW<i class="w-icon-long-arrow-right"></i>
+                            </a>
+                        </div>
+                        <!-- End of .banner-content -->
+                    </div>
+                    <!-- End of .container -->
+                </div>
+                <!-- End of .intro-slide2 -->
+
+                <div class="swiper-slide banner banner-fixed intro-slide intro-slide3" :style="{ backgroundImage: 'url(' + require('@/assets/images/demos/demo1/sliders/slide-3.jpg') + ')' }">
+                    <div class="container">
+                        <figure class="slide-image skrollable slide-animate" data-animation-options="{
+                            'name': 'fadeInDownShorter',
+                            'duration': '1s'
+                        }">
+                            <img src="~/assets/images/demos/demo1/sliders/skate.png" alt="Banner"
+                                data-bottom-top="transform: translateY(10vh);"
+                                data-top-bottom="transform: translateY(-10vh);" width="310" height="444">
+                        </figure>
+                        <div class="banner-content text-right y-50">
+                            <p class="font-weight-normal text-default text-uppercase mb-0 slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInLeftShorter',
+                                'duration': '1s',
+                                'delay': '.6s'
+                            }">
+                                Top weekly Seller
+                            </p>
+                            <h5 class="banner-subtitle font-weight-normal text-default ls-25 slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInLeftShorter',
+                                'duration': '1s',
+                                'delay': '.4s'
+                            }">
+                                Trending Collection
+                            </h5>
+                            <h3 class="banner-title p-relative font-weight-bolder ls-50 slide-animate"
+                                data-animation-options="{
+                                'name': 'fadeInLeftShorter',
+                                'duration': '1s',
+                                'delay': '.2s'
+                            }"><span class="text-white mr-4">Roller</span>-skate
+                            </h3>
+                            <div class="btn-group slide-animate" data-animation-options="{
+                                'name': 'fadeInLeftShorter',
+                                'duration': '1s',
+                                'delay': '.8s'
+                            }">
+                                <a href="shop-list.html"
+                                    class="btn btn-dark btn-outline btn-rounded btn-icon-right">SHOP
+                                    NOW<i class="w-icon-long-arrow-right"></i></a>
+                            </div>
+                            <!-- End of .banner-content -->
+                        </div>
+                        <!-- End of .container -->
+                    </div>
+                </div>
+                <!-- End of .intro-slide3 -->
+            </div>
+            <div class="swiper-pagination"></div>
+            <button class="swiper-button-next"></button>
+            <button class="swiper-button-prev"></button>
+        </div>
+        <!-- End of .swiper-container -->
+    </section>
+</template>
+
+<script>
+    // import Swiper JS
+    // add or remove unused modules
+    import { Swiper, Navigation, Autoplay } from 'swiper'
+    import '~/assets/vendor/swiper/swiper-bundle.min.css'
+    export default {
+        
+        mounted() {
+            // configure Swiper to use modules. The modules were tested with SwiperJS v6.8.4 with NuxtJS v2.15.7
+            // previously it was before export default. Moved here for performance issues. Move back in case of problems.
+            // add or remove unused modules
+            Swiper.use([Navigation, Autoplay, ])
+            
+            // init Swiper:
+            const swiper = new Swiper('.animation-slider', {
+                // Optional parameters
+                // @see https://swiperjs.com/swiper-api#parameters
+                direction: 'horizontal',
+                slidesPerView: 1,
+                loop: true,
+                // remove unused modules if needed
+                modules: [Navigation, Autoplay],
+
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },             
+                // Autoplay if needed
+                autoplay: {
+                    delay: 8000,
+                    disableOnInteraction: false
+                },
+                
+                // Configure other options. Not tested
+            })
+        }
+    }
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-wrapper">
+    <Header />
+      <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "default"
+  }
+</script>
